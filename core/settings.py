dffb686040eeca0c1f621aaae4acfd80c7bcccdd
owoicho <michaelogaje033@gmail.com>
2025-07-20 15:16:08 +0100

    postgres db

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 import environ
 
+import dj_database_url
+import os
 
 
 
@@ -111,12 +113,16 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
 
 
 # Password validation
